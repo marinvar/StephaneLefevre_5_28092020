39173b60317b67b3cc71287688be135a65557517
HO.SLEFEVRE <ho.slefevre@cma-cgm.com>
2021-01-23 20:46:45 +0100

    js and html validations done

--- src/formControls.js
+++ src/formControls.js
@@ -1,46 +1,35 @@
+let orderForm = document.getElementById('order-form');
 let firstName = document.getElementById('first-name');
 let lastName = document.getElementById('last-name');
 let address = document.getElementById('address');
 let city = document.getElementById('city');
 let email = document.getElementById('email');
-let orderButton = document.getElementById('orderButton');
+let elements = [firstName, lastName, address, city, email];
 
 const addRegexControl = (element, regX) => {
-  /* let element = document.getElementById(el); */
   element.addEventListener('input', function (event) {
-    let elRGEX = regX;
-    let result = elRGEX.exec(event.target.value);
-    if (result === null && element !== email) {
+    let result = regX.exec(event.target.value);
+    if (result === null) {
       element.classList.add('is-invalid');
-      /* orderButton.disabled = true; */
-    } else if (result === null && element === email) {
-      element.classList.add('is-invalid-email');
-      /* orderButton.disabled = true; */
-    } else if (result !== null && element.classList.contains('is-invalid') && element !== email) {
+    } else if (result !== null && element.classList.contains('is-invalid')) {
       element.classList.remove('is-invalid');
-      /* orderButton.disabled = false; */
-    } else if (result !== null && element.classList.contains('is-invalid-email') && element === email) {
-      element.classList.remove('is-invalid-email');
-      /* orderButton.disabled = false; */
     }
-    isAllValid();
-  });
+  }, false);
 }
 
-const isAllValid = () => {
-  const elements = [firstName, lastName, address, city, email];
-  let allValid = true;
-  for (let element of elements) {
-    if (element.value.length === 0 || element.classList.contains('is-invalid') || element.classList.contains('is-invalid-email')) {
-      allValid = false;
+const addValidationListener = () => {
+  orderForm.addEventListener('submit', function (event) {
+    let validation = true;
+    for (let element of elements) {
+      if (element.classList.contains('is-invalid')) {
+        validation = false;
+      }
     }
-  }
-  console.log(allValid);
-  if (allValid && orderButton.disabled === true) {
-    orderButton.disabled = false;
-  } else if (!allValid && orderButton.disabled === false) {
-    orderButton.disabled = true;
-  };
+    if (validation === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      }
+  }, false);
 }
 
 export const addRegexControls = () => {
@@ -48,7 +37,7 @@ export const addRegexControls = () => {
   addRegexControl(lastName, /^[A-Za-zÀ-ÖØ-öø-ÿ-\s']{1,20}$/);
   addRegexControl(address, /^[A-Za-z0-9À-ÖØ-öø-ÿ-\s,\.']{1,100}$/);
   addRegexControl(city, /^[A-Za-zÀ-ÖØ-öø-ÿ-\s']{1,50}$/);
-  /* addRegexControl(email, /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/); */
   addRegexControl(email, /^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+  addValidationListener();
 }
 
